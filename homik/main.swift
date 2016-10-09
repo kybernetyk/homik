c1620699082c962c93e71d4d318935c731c1c10d
kyb <js@suborbital.io>
2016-10-09 15:16:09 +0200

    Initial Commit

---
+++ homik/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  homik
+//
+//  Created by kyb on 09/10/2016.
+//  Copyright © 2016 Suborbital Softowrks Ltd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
